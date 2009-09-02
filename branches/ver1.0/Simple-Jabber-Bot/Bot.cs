37de2fced1ca9ece77a85d5b1aed0268ef6fbfcf
kirill.temnenkov <kirill.temnenkov@d0762ade-971a-11de-8bb7-81ed9af7660d>
2009-09-02 17:20:02 +0000

    new review: owner: kirill.temnenkov Just logging of dummy Bot using log4net.

---
+++ branches/ver1.0/Simple-Jabber-Bot/Bot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Temnenkov.SimpleJabberBot
+{
+    internal class Bot
+    {
+        internal void Connect()
+        {
+            Logger.Log(LogType.Info, "Connect");
+        }
+
+        internal void Disconnect()
+        {
+            Logger.Log(LogType.Info, "Disconnect");
+        }
+    }
+}
---
+++ branches/ver1.0/Simple-Jabber-Bot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Temnenkov.SimpleJabberBot
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Logger.Log(LogType.Info, "Starting in console mode");
+
+            var bot = new Bot();
+            bot.Connect();
+
+            Console.WriteLine("Type \"quit\" to end.");
+            while (!Console.ReadLine().Equals("quit", StringComparison.InvariantCultureIgnoreCase))
+                Console.WriteLine("Type \"quit\" to end.");
+
+            Logger.Log(LogType.Info, "Quitting from console mode");
+            bot.Disconnect();
+        }
+    }
+}
