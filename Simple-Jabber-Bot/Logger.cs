e3fbdb0826509c6d6c124d18217d55bb06a28e90
kirill.temnenkov <kirill.temnenkov@d0762ade-971a-11de-8bb7-81ed9af7660d>
2009-09-02 17:20:02 +0000

    new review: owner: kirill.temnenkov Just logging of dummy Bot using log4net.

---
+++ Simple-Jabber-Bot/Logger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using log4net.Config;
+
+[assembly: XmlConfigurator(Watch = true)]
+namespace Temnenkov.SimpleJabberBot
+{
+    internal static class Logger
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Bot));
+
+        internal static void Log(LogType type, string message)
+        {
+            Log(type, message, null);
+        }
+
+        internal static void Log(LogType type, string message, Exception ex)
+        {
+            switch (type)
+            {
+                case LogType.Debug:
+                    log.Debug(message, ex);
+                    break;
+                case LogType.Error:
+                    log.Error(message, ex);
+                    break;
+                case LogType.Fatal:
+                    log.Fatal(message, ex);
+                    break;
+                case LogType.Info:
+                    log.Info(message, ex);
+                    break;
+                case LogType.Warn:
+                    log.Warn(message, ex);
+                    break;
+            }
+        }
+
+    }
+
+    internal enum LogType
+    {
+        Debug,
+        Warn,
+        Error,
+        Info,
+        Fatal
+    }
+}
