3e03ee97aa202f2e98e6f4e92d604d87e3232bcb
kirill.temnenkov <kirill.temnenkov@d0762ade-971a-11de-8bb7-81ed9af7660d>
2009-09-08 02:06:40 +0000

    new review: owner: kirill.temnenkov Very-very simple extracting log from database Refactoring

---
+++ branches/ver1.0/ExtractLog/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Temnenkov.SJB.Common;
+
+namespace Temnenkov.SJB.ExtractLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var selectDate = DateTime.Now.AddDays(-1);
+            var firstDate = selectDate.Date;
+            var secondDate = selectDate.Date.AddDays(1).AddMilliseconds(-1);
+
+            using (var db = new Database.Database("Log"))
+            {
+                var resFile = Path.Combine(Path.GetDirectoryName(Utils.GetExecutablePath()), "exp");
+                using (var sw = new System.IO.StreamWriter(resFile, false, Encoding.GetEncoding(1251)))
+                {
+                    using (var reader = db.ExecuteReader("SELECT [Date], [From], [Message] from [Log] WHERE [Date] BETWEEN ? AND ? AND [Jid] = 'fido828@conference.jabber.ru' ORDER BY [Id]",
+                        firstDate, secondDate))
+                    {
+                        while (reader.Read())
+                        {
+                            sw.WriteLine(string.Format("{0} {1} {2}",
+                                reader.GetDateTime(0).ToShortTimeString(),
+                                reader.GetString(1),
+                                reader.GetString(2)));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
