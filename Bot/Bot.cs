709855b9ca06d9ee274cbae08fedec1340748fb0
kirill.temnenkov <kirill.temnenkov@d0762ade-971a-11de-8bb7-81ed9af7660d>
2009-09-17 14:06:05 +0000

    log & ping in private messages

--- Bot/Bot.cs
+++ Bot/Bot.cs
@@ -66,6 +66,25 @@ namespace Temnenkov.SJB.Bot
                     }
 
                     break;
+                case MessageType.chat:
+                    Logger.Log(LogType.Info, String.Format("chat message received from resource {0} in room {1}: {2}", msg.From.Resource, msg.From.Bare, msg.Body));
+                    if (!string.IsNullOrEmpty(msg.Body) && msg.Body.Equals("ping", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Logger.Log(LogType.Info, String.Format("Pinging back to {0}", msg.From.User));
+                        if (msg.From.Bare != null && msg.From.Bare == _room.JID.Bare)
+                            // значит, в комнате
+                            SendPrivateMessage(msg.From.Resource, String.Format("Hey {0}, it's {1}.", msg.From.Resource, DateTime.Now));
+                        else // не в комнате
+                            SendMessage(msg.From.Bare, String.Format("Hey {0}, it's {1}.", msg.From.Bare, DateTime.Now));
+                    }
+
+                    if (!string.IsNullOrEmpty(msg.Body) && msg.Body.Equals("log", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Logger.Log(LogType.Info, String.Format("Send log to {0}", msg.From.User));
+                        if (msg.From.Bare != null && msg.From.Bare == _room.JID.Bare) // логи только через приватное сообщение из комнаты
+                            SendLog(msg.From.Bare, msg.From.Resource);
+                    }
+                    break;
                 default:
                     Logger.Log(LogType.Info, String.Format("Message received from {0}@{1}: {2}", msg.From.User, msg.From.Server, msg.Body));
                     break;
@@ -76,6 +95,11 @@ namespace Temnenkov.SJB.Bot
         {
             if (_room != null && _room.IsParticipating)
                 _room.PrivateMessage(to, message);
+        }
+
+        private void SendMessage(string to, string message)
+        {
+            _client.Message(to, message);
         }
 
         private void SendLog(string jid, string to)
@@ -176,6 +200,11 @@ namespace Temnenkov.SJB.Bot
 
             var result = _room.IsParticipating;
 
+            _room.OnParticipantJoin += new RoomParticipantEvent(_room_OnParticipantJoin);
+            _room.OnParticipantLeave += new RoomParticipantEvent(_room_OnParticipantLeave);
+            _room.OnPrivateMessage += new MessageHandler(_room_OnPrivateMessage);
+            _room.OnRoomMessage += new MessageHandler(_room_OnRoomMessage);
+
             if (result)
                 Logger.Log(LogType.Info, String.Format("Join in room {0} sucessfully", jid));
             else
@@ -184,11 +213,32 @@ namespace Temnenkov.SJB.Bot
             return result;
         }
 
+        void _room_OnRoomMessage(object sender, Message msg)
+        {
+            Logger.Log(LogType.Info, string.Format("room message {0}", msg));
+        }
+
+        void _room_OnPrivateMessage(object sender, Message msg)
+        {
+            Logger.Log(LogType.Info, string.Format("private message {0}", msg));
+        }
+
+        void _room_OnParticipantLeave(Room room, RoomParticipant participant)
+        {
+            Logger.Log(LogType.Info, string.Format("leave {0}", participant.Nick));
+        }
+
+        void _room_OnParticipantJoin(Room room, RoomParticipant participant)
+        {
+            Logger.Log(LogType.Info, string.Format("join {0}", participant.Nick));
+        }
+
     }
 }
 
 
-//toDo log & ping in private messages
+//toDO refactor log & ping in private messages
+//toDo join leave etc
 //toDo prevent doubling of delayed messages
 //toDo prevent kick
 //toDo multiple channels
