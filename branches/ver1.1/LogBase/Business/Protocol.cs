536f1ad6b39653ccb86a251fe67f64ec8059750d
kirill.temnenkov <kirill.temnenkov@d0762ade-971a-11de-8bb7-81ed9af7660d>
2009-10-01 13:28:52 +0000

    log

---
+++ branches/ver1.1/LogBase/Business/Protocol.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Temnenkov.SJB.Common;
+
+namespace Temnenkov.SJB.LogBase.Business
+{
+    public class Protocol 
+    {
+        private List<PersistentLine> Lines {get; set;}
+
+        private Protocol()
+        {
+            Lines = new List<PersistentLine>();
+        }
+
+        public static Protocol Load(PersistentLineDataLayer dal, string jid,
+            DateTime perBeg, DateTime perEnd)
+        {
+            var result = new Protocol();
+            dal.Load(result.Lines, jid, perBeg, perEnd);
+            return result;
+        }
+
+        public string Export(bool withDate)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < Lines.Count - 1; ++i)
+                sb.AppendLine(Lines[i].DisplayString(withDate));
+            return sb.ToString();
+        }
+
+    }
+}
