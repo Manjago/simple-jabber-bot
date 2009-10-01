bbced1148653815d7baf6b94d50b90266ed881ab
kirill.temnenkov <kirill.temnenkov@d0762ade-971a-11de-8bb7-81ed9af7660d>
2009-10-01 13:20:56 +0000

    log function (not tested)

--- branches/ver1.1/PingerPlugin/Ping.cs
+++ branches/ver1.1/PingerPlugin/Ping.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Temnenkov.SJB.Common;
+using Temnenkov.SJB.LogBase.Business;
 
 namespace Temnenkov.SJB.PingerPlugin
 {
@@ -17,17 +18,32 @@ namespace Temnenkov.SJB.PingerPlugin
 
         private static string RoomHelpMessage(string to)
         {
-            return string.Format("Привет, {0}! Используй команды \"ping\", \"log\", \"help\".", to);
+            return string.Format("Привет, {0}! Используй команды \"zog\",\"ping\", \"log\", \"help\".", to);
         }
 
         private static string NormalHelpMessage(string to)
         {
-            return string.Format("Привет, {0}! Используй команды \"ping\", \"help\".", to);
+            return string.Format("Привет, {0}! Используй команды \"zog\",\"ping\", \"help\".", to);
         }
 
         private static string PingMessage(string to)
         {
             return string.Format("Привет {0}, сейчас {1}.", to, DateTime.Now);
+        }
+
+        private static string ZogMessage(string to)
+        {
+            return string.Format("Помни, {0}, что ЗОГ не дремлет, ЗОГ не спит!", to);
+        }
+
+        private static string LogMessage(string jid)
+        {
+            var dal = new PersistentLineDataLayer();
+
+            return Protocol.Load(new PersistentLineDataLayer(), 
+                jid,
+                DateTime.Now.AddDays(-1),
+                DateTime.Now.AddDays(1)).Export(false); 
         }
 
         private static bool IsCommand(string message, string cmd)
@@ -41,6 +57,8 @@ namespace Temnenkov.SJB.PingerPlugin
                 Translator.SendNormalMessage(e.From, NormalHelpMessage(e.From));
             if (IsCommand(e.Message, "ping"))
                 Translator.SendNormalMessage(e.From, PingMessage(e.From));
+            if (IsCommand(e.Message, "zog"))
+                Translator.SendNormalMessage(e.From, ZogMessage(e.From));
             if (IsCommand(e.Message, "shutdown") && 
                 !string.IsNullOrEmpty(OperatorJid) && 
                 OperatorJid.Equals(e.From, StringComparison.InvariantCultureIgnoreCase))
@@ -56,6 +74,12 @@ namespace Temnenkov.SJB.PingerPlugin
 
             if (IsCommand(e.Message, "ping"))
                 Translator.SendRoomPrivateMessage(e.RoomJid, e.From, PingMessage(e.From));
+
+            if (IsCommand(e.Message, "zog"))
+                Translator.SendRoomPrivateMessage(e.RoomJid, e.From, ZogMessage(e.From));
+
+            if (IsCommand(e.Message, "log"))
+                Translator.SendRoomPrivateMessage(e.RoomJid, e.From, LogMessage(e.RoomJid));
         }
 
         void Translator_RoomMessage(object sender, RoomMessageEventArgs e)
@@ -66,6 +90,10 @@ namespace Temnenkov.SJB.PingerPlugin
                 Translator.SendRoomPublicMessage(e.RoomJid, RoomHelpMessage(e.From));
             if (IsCommand(e.Message, "ping"))
                 Translator.SendRoomPrivateMessage(e.RoomJid, e.From, PingMessage(e.From));
+            if (IsCommand(e.Message, "zog"))
+                Translator.SendRoomPublicMessage(e.RoomJid, ZogMessage(e.From));
+            if (IsCommand(e.Message, "log"))
+                Translator.SendRoomPrivateMessage(e.RoomJid, e.From, LogMessage(e.RoomJid));
         }
 
     }
