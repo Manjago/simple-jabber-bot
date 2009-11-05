41eb02501d37c306cc736011a74f82ceddc2e72f
kirill.temnenkov <kirill.temnenkov@d0762ade-971a-11de-8bb7-81ed9af7660d>
2009-11-05 18:41:17 +0000

    Service mode (beta)

---
+++ Bot/BotAsService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+using Temnenkov.SJB.Common;
+
+namespace Temnenkov.SJB.Bot
+{
+    partial class BotAsService : ServiceBase
+    {
+        private Bot _bot;
+
+        public BotAsService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Logger.Log(LogType.Info, "Starting in service mode");
+
+            _bot = new Bot();
+            if (_bot.Connect())
+            {
+                Logger.Log(LogType.Info, "Successfully connected");
+
+                if (!_bot.JoinRoom(string.Format("{0}/{1}", Settings.RoomJid, Settings.NameInRoom)))
+                {
+                    Logger.Log(LogType.Error, "Fail join room");
+                    Stop();
+                }
+            }
+            else
+            {
+                Logger.Log(LogType.Error, "Fail connect");
+                Stop();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            _bot.Disconnect();
+        }
+    }
+}
